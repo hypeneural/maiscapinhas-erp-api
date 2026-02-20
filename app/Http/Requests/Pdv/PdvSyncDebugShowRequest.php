05674564b907f4a44e77feee1ebcc5232e3c6373
Hype Neural <hypeneural@gmail.com>
2026-02-20 16:28:45 -0300

    v200226-1628

--- app/Http/Requests/Pdv/PdvSyncDebugShowRequest.php
+++ app/Http/Requests/Pdv/PdvSyncDebugShowRequest.php
@@ -21,9 +21,53 @@ class PdvSyncDebugShowRequest extends FormRequest
         ];
     }
 
+    protected function prepareForValidation(): void
+    {
+        $merged = [];
+
+        if ($this->has('include_raw')) {
+            $merged['include_raw'] = $this->normalizeBooleanInput($this->input('include_raw'));
+        }
+
+        if ($this->has('include_decoded')) {
+            $merged['include_decoded'] = $this->normalizeBooleanInput($this->input('include_decoded'));
+        }
+
+        if ($merged !== []) {
+            $this->merge($merged);
+        }
+    }
+
+    private function normalizeBooleanInput(mixed $value): mixed
+    {
+        if (is_bool($value)) {
+            return $value;
+        }
+
+        if (is_int($value)) {
+            if ($value === 1) {
+                return true;
+            }
+            if ($value === 0) {
+                return false;
+            }
+        }
+
+        if (is_string($value)) {
+            $normalized = strtolower(trim($value));
+            if (in_array($normalized, ['1', 'true', 'yes', 'on'], true)) {
+                return true;
+            }
+            if (in_array($normalized, ['0', 'false', 'no', 'off'], true)) {
+                return false;
+            }
+        }
+
+        return $value;
+    }
+
     public function queryParameters(): array
     {
         return [];
     }
 }
-
